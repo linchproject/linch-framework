5f95280de92ee77f303a9282d91e47bea8231365
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-12 19:30:30 +0100

    add dependency to linch-http and method to get an i18n object from request locale

--- src/main/java/com/linchproject/framework/Controller.java
+++ src/main/java/com/linchproject/framework/Controller.java
@@ -5,11 +5,16 @@ import com.linchproject.core.Result;
 import com.linchproject.core.Route;
 import com.linchproject.framework.assets.AssetService;
 import com.linchproject.framework.db.ConnectionService;
+import com.linchproject.framework.i18n.I18n;
 import com.linchproject.framework.i18n.I18nService;
 import com.linchproject.framework.view.RenderService;
+import com.linchproject.http.CookieService;
+import com.linchproject.http.LocaleService;
+import com.linchproject.http.SessionService;
 
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -21,6 +26,12 @@ public abstract class Controller extends com.linchproject.core.Controller {
     protected RenderService renderService;
     protected I18nService i18nService;
     protected AssetService assetService;
+
+    protected SessionService sessionService;
+    protected CookieService cookieService;
+    protected LocaleService localeService;
+
+    private I18n i18n;
 
     protected Result render(String template) {
         return render(template, context());
@@ -63,6 +74,14 @@ public abstract class Controller extends com.linchproject.core.Controller {
         return inputStream != null? binary(inputStream): error("");
     }
 
+    protected I18n getI18n() {
+        if (i18n == null) {
+            Locale locale = localeService.getLocale();
+            i18n = locale != null? i18nService.getI18n(locale): i18nService.getI18n(Locale.getDefault());
+        }
+        return i18n;
+    }
+
     public void setConnectionService(ConnectionService connectionService) {
         this.connectionService = connectionService;
     }
@@ -78,4 +97,17 @@ public abstract class Controller extends com.linchproject.core.Controller {
     public void setAssetService(AssetService assetService) {
         this.assetService = assetService;
     }
+
+    public void setSessionService(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    public void setCookieService(CookieService cookieService) {
+        this.cookieService = cookieService;
+    }
+
+    public void setLocaleService(LocaleService localeService) {
+        this.localeService = localeService;
+    }
+
 }
