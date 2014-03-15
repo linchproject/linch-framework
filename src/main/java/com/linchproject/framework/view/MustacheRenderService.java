d8b35e0ace85663ead3f5026e75d584153b8831b
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-15 21:06:04 +0100

    normalize mustache file name

--- src/main/java/com/linchproject/framework/view/MustacheRenderService.java
+++ src/main/java/com/linchproject/framework/view/MustacheRenderService.java
@@ -3,6 +3,7 @@ package com.linchproject.framework.view;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
+import com.google.common.io.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +29,8 @@ public class MustacheRenderService implements RenderService {
         MustacheFactory mf = new DefaultMustacheFactory() {
             @Override
             public Reader getReader(String resourceName) {
+                resourceName = Files.simplifyPath(resourceName);
+
                 ClassLoader contextClassLoader = null;
                 if (classLoader != null) {
                     log.debug("using {}", classLoader);
